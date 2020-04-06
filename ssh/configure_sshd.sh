8ca4b7e8e0a94ecc1da9847b511f6f9446166afe
Ross Harper <rossharper@users.noreply.github.com>
2020-04-06 16:27:17 +0100

    Reset SSH port to 22

--- ssh/configure_sshd.sh
+++ ssh/configure_sshd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo sed -i.bak -e 's/^#\?Port\s\+.*/Port 1212/' \
- -e 's/^#\?PasswordAuthentication\s\+.*/PasswordAuthentication no/' \
+sudo sed -i.bak -e 's/^#\?PasswordAuthentication\s\+.*/PasswordAuthentication no/' \
  -e 's/^#\?UsePAM\s.*/UsePAM no/' \
  -e 's/^#\?PermitRootLogin\s\+.*/PermitRootLogin no/' /etc/ssh/sshd_config
