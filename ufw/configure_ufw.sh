f61caf2ade29eb15f5e5f0ef685ee20671f55a4a
Ross Harper <rossharper@users.noreply.github.com>
2020-04-06 17:08:52 +0100

    Add ssh limit rule

--- ufw/configure_ufw.sh
+++ ufw/configure_ufw.sh
@@ -3,6 +3,8 @@
 # Enable IPv6 (to be sure)
 sudo sed -i.bak -e 's/^#\?IPV6=.*/IPV6=yes/' /etc/default/ufw
 
-sudo ufw allow ssh
+sudo ufw allow ssh/tcp
+
+sudo ufw limit ssh/tcp
 
 sudo ufw --force enable
