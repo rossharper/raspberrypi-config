efcfa8622466aa45a8eae416638344dcc166e74a
Ross Harper <rossharper@users.noreply.github.com>
2020-05-02 13:23:28 +0100

    DDclient setup

--- ddclient/configure_ddclient.sh
+++ ddclient/configure_ddclient.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+sudo apt install libio-socket-ssl-perl
+
 sudo apt install ddclient
+
+sudo sed -i.bak -e 's/^protocol=dyndns2/protocol=dyndns2\nssl=yes\nuse=web, web=checkip.dynu.com\/, web-skip="IP Address"\ndaemon=60\nsyslog=yes/' /etc/ddclient.conf
+
+sudo sed -i.bak -e 's/^run_daemon="false"/run_daemon="true"/' /etc/default/ddclient
